9c2308ae5244bd63301325d0cd6abb93d44ee1c9
tawhidshaikh <93393114+tawhidshaikh@users.noreply.github.com>
2023-02-23 17:15:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const { Server } = require("socket.io");
+const io = new Server(8000, { /* options */ });
+const users = {};
+
+io.on('connection', socket =>{
+    socket.on('new-user-joined', name =>{
+        // console.log("New user", name)
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined', name);
+    });
+    
+    socket.on('send', message =>{
+        socket.broadcast.emit('receive', {message: message, name: users[socket.id]})
+    });
+
+    socket.on('disconnect', message =>{
+        socket.broadcast.emit('left', users[socket.id]);
+        delete users[socket.id];
+    });
+
+
+})
